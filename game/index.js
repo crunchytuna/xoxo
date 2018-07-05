b0f93f0a1f91b26883718013e95843c8ce285557
Jessica Smith <smith.j.m.13@gmail.com>
2018-07-05 14:42:16 -0500

    your turn

--- game/index.js
+++ game/index.js
@@ -16,7 +16,6 @@ export const move = (turn, position) => ({
 });
 
 export default function reducer(state = { board: board, turn: 'X' }, action) {
-  // TODO
   if (action.type === 'move') {
     return {
       board: state.board.setIn(action.position, action.turn),
@@ -25,8 +24,70 @@ export default function reducer(state = { board: board, turn: 'X' }, action) {
   } else return state;
 }
 
-// const winner = function () {
-//   streak(00,01,02)
-//   streak(10,11,12)
 
-// }
+const streak = (board, firstCoord, ...remainingCoords) => {
+  let xCount = 0;
+  let oCount = 0;
+
+  for (let i = 1; i < arguments.length; i++) {
+    if (board.getIn(arguments[i]) === 'X') {
+      xCount++;
+      if (xCount === 3) return 'X';
+    } else if (board.getIn(arguments[i]) === 'O') {
+      oCount++;
+      if (oCount === 3) return 'O';
+    }
+  }
+  return undefined;
+};
+
+const winner = function(board) {
+  let result = streak(board, [0, 0], [0, 1], [0, 2]);
+  if (result) {
+    return result;
+  }
+
+  result = streak(board, [1, 0], [1, 1], [1, 2]);
+  if (result) {
+    return result;
+  }
+  result = streak(board, [2, 0], [2, 1], [2, 2]);
+  if (result) {
+    return result;
+  }
+  result = streak(board, [0, 0], [1, 0], [2, 0]);
+  if (result) {
+    return result;
+  }
+
+  result = streak(board, [0, 1], [1, 1], [2, 1]);
+  if (result) {
+    return result;
+  }
+
+  result = streak(board, [0, 2], [1, 2], [2, 2]);
+  if (result) {
+    return result;
+  }
+
+  result = streak(board, [0, 0], [1, 1], [2, 2]);
+  if (result) {
+    return result;
+  }
+  result = streak(board, [0, 2], [1, 1], [2, 0]);
+  if (result) {
+    return result;
+  }
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; i++) {
+      if (board.hasIn([i][j]) === null) {
+        return null;
+      }
+    }
+  }
+
+
+  return 'draw'
+
+};
