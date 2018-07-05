af002bae5b5ac23030c316cc06952b8fbcf015c0
Jehoshuah K <musicalbard@gmail.com>
2018-07-05 13:33:29 -0500

    game is functional

--- game/index.js
+++ game/index.js
@@ -1,6 +1,32 @@
-import {Map} from 'immutable'
+import { Map } from 'immutable';
+let board = Map();
 
-export default function reducer(state, action) {
+const flip = function(turn) {
+  if (turn === 'X') {
+    return 'O';
+  } else return 'X';
+};
+
+// action creator
+
+export const move = (turn, position) => ({
+  type: 'move',
+  position: position,
+  turn: turn,
+});
+
+export default function reducer(state = { board: board, turn: 'X' }, action) {
   // TODO
-  return state
+  if (action.type === 'move') {
+    return {
+      board: state.board.setIn(action.position, action.turn),
+      turn: flip(state.turn),
+    };
+  } else return state;
 }
+
+// const winner = function () {
+//   streak(00,01,02)
+//   streak(10,11,12)
+
+// }
